88466f37c00038cb89db455f38dcb4f424955b4d
Muhammad Jundy Rabbani <jundyfauzi789@gmail.com>
2026-02-16 14:17:08 +0700

    chore: remove hardcoded prerender origin

--- svelte.config.js
+++ svelte.config.js
@@ -5,10 +5,7 @@ import preprocess from 'svelte-preprocess';
 const config = {
     preprocess: preprocess(),
     kit: {
-        adapter: adapter(),
-        prerender: {
-            origin: "https://segs.lol",
-        }
+        adapter: adapter()
     },
 };
 
